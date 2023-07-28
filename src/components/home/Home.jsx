c0388242f524abbd75033436972bb06dc08ee57c
moonnada <kimbm2014@gmail.com>
2023-07-28 02:13:43 -0700

    Add widgets on dashboard

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,13 +4,25 @@ import { signInWithPopup } from 'firebase/auth';
 import { auth } from '../../firebase';
 import Sidebar from '../sidebar/Sidebar';
 import Navbar from '../navbar/Navbar';
+import Widget from '../widget/Widget';
+import Featured from '../featured/Featured';
+import Chart from '../chart/Chart';
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className='homeContainer'>
-      <Navbar />
-      home container
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user"/>
+          <Widget />
+          <Widget />
+          <Widget />
+        </div>
+        <div className='charts'>
+          <Featured />
+          <Chart />
+        </div>
       </div>
     </div>
   );
